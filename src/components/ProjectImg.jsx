08c5f40b4eb2675e3cdaf576e3acc08827e66c78
amanrsayyad <79994006+amanrsayyad@users.noreply.github.com>
2024-01-25 21:06:53 +0530

    Changes

--- src/components/ProjectImg.jsx
+++ src/components/ProjectImg.jsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { HiArrowLongRight } from "../utils/Icons";
 import projectBg2 from "../assets/Images/projectBg2.jpg";
 import projectBg5 from "../assets/Images/projectBg5.jpg";
 import projectBg6 from "../assets/Images/projectBg6.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const ProjectImg = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <ProjectContainer>
       <ProjectGrid>
-        <ProjectCard>
+        <ProjectCard data-aos="zoom-out-left" data-aos-delay="50">
           <img src={projectBg2} alt="" />
         </ProjectCard>
-        <ProjectCard2>
+        <ProjectCard2 data-aos="zoom-out-left" data-aos-delay="100">
           <img src={projectBg6} alt="" />
         </ProjectCard2>
-        <ProjectCard3>
+        <ProjectCard3 data-aos="zoom-out-left" data-aos-delay="150">
           <img src={projectBg5} alt="" />
         </ProjectCard3>
       </ProjectGrid>
